2e1d5062a01cdcfd031a7c1a14ec12e71b037e8d
abhishek <ak4290104@gmail.com>
2022-02-16 21:36:08 +0800

    BizCard App

---
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bizcard.ui.theme.BizCardTheme
+
+// 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 - git token
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BizCardTheme {
+        Greeting("Android")
+    }
+}
